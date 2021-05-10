87f8e7fb59297a299b2b68529b88f6d66006bb24
Farhan <mfarhanali@live.com>
2021-05-10 19:45:43 +0000

    list mixed lines

---
+++ lab_input/iptaker01.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+test = input("Please enter an IPv4 IP address:")
+print("You told me the IPv4 address is:" + test)
+
+
+
---
+++ lab_input/iptaker02.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+vendor_name = input("vandor name: ")
+print("Vendor name is: ", vendor_name)
+
+
+
+
+
